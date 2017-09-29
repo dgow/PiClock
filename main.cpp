29a6ddbdca185256919b177028604d10605deecb
Johann Bauer <johann.bauer1@gmail.com>
2017-09-29 10:24:58 +0200

    enable fullscreen

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
+#include <QHostInfo>
 
 int main(int argc, char *argv[])
 {
     //Q_INIT_RESOURCE(resources);
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowState(Qt::WindowFullScreen);
+
+    qDebug() << QHostInfo::localHostName();
+
+    if( QHostInfo::localHostName() != "johanns-imac")
+    {
+        w.setWindowState(Qt::WindowFullScreen);
+    }
     w.show();
 
     return a.exec();
