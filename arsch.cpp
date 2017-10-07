fd66f1eff850a4158583df790087249aa96f6a08
Johann Bauer <johann.bauer1@gmail.com>
2017-10-07 15:51:31 +0200

    push

--- arsch.cpp
+++ arsch.cpp
@@ -7,6 +7,9 @@ Arsch::Arsch(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Arsch)
 {
+    hour = 0;
+    minute = 0;
+
     ui->setupUi(this);
     ui->timeLabel->setAttribute(Qt::WA_TranslucentBackground);
 }
@@ -16,10 +19,59 @@ Arsch::~Arsch()
     delete ui;
 }
 
+void Arsch::UpdateTime()
+{
+    if(hour > 24)
+    {
+        hour = 0;
+    }
+
+    if(hour < 0 )
+    {
+        hour = 23;
+    }
+
+    if(minute > 59)
+    {
+        minute = 0;
+    }
+
+    if(minute < 0 )
+    {
+        minute = 59;
+    }
+
+
+    ui->timeLabel->setText(QString::number(this->hour) + " : " + QString::number(this->minute));
+}
+
 void Arsch::on_pushButton_clicked()
 {
-    qDebug() << "fotze fotze";
     emit this->GoBack();
+}
+
+void Arsch::on_hourDown_clicked()
+{
+    this->hour--;
+    this->UpdateTime();
 
 }
 
+void Arsch::on_hourUp_clicked()
+{
+    this->hour++;
+    this->UpdateTime();
+
+}
+
+void Arsch::on_minuteDown_clicked()
+{
+    this->minute--;
+    this->UpdateTime();
+}
+
+void Arsch::on_minuteUp_clicked()
+{
+    this->minute++;
+    this->UpdateTime();
+}
--- arsch.h
+++ arsch.h
@@ -21,8 +21,21 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_hourDown_clicked();
+
+    void on_hourUp_clicked();
+
+    void on_minuteDown_clicked();
+
+    void on_minuteUp_clicked();
+
 private:
+    void UpdateTime();
+
     Ui::Arsch *ui;
+
+    int hour;
+    int minute;
 };
 
 #endif // ARSCH_H
