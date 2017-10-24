4faca81f2c01b381ed4890bbc62b39e6abb9a511
Johann Bauer <johann.bauer1@gmail.com>
2017-10-24 21:03:40 +0200

    In computing, the mean time to failure keeps getting shorter.

--- volumeknobthread.cpp
+++ volumeknobthread.cpp
@@ -94,7 +94,7 @@ void VolumeKnobThread::run()
             {
                 qDebug() << "Louder!!!";
                 this->SwitchState(KnobInit);
-                QThread::sleep(0.2);
+                //QThread::sleep(0.2);
                 this->SwitchState(KnobInit);
             }
             break;
@@ -106,7 +106,7 @@ void VolumeKnobThread::run()
             {
                 qDebug() << "Leiser!!!";
                 this->SwitchState(KnobInit);
-                QThread::sleep(1);
+                //QThread::sleep(1);
                 this->SwitchState(KnobInit);
 
             }
