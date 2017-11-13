dec4d3dfeddc24b388c05ea5411c8a2c574cb562
Johann Bauer <johann.bauer1@gmail.com>
2017-11-13 13:36:07 +0100

    Test-tube babies shouldn't throw stones.

--- mopidyreader.h
+++ mopidyreader.h
@@ -7,18 +7,22 @@
 #include <QtWebSockets/QWebSocket>
 #include <QMetaEnum>
 
+enum MessageId {
+        Play = 666,
+        Next = 777
+};
+
 class MopidyReader : public QObject
 {
     Q_OBJECT
+    Q_ENUM(MessageId)
+
 public:
     explicit MopidyReader(QObject *parent);
 
 
-    enum MessageId {
-            Play = 666,
-            Next = 777
-    };
-    //Q_ENUM(MessageId);
+
+    //
 
     QString title;
     QString artist;
