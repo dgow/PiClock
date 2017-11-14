3ed94471361bd16a55fd04514339b96bb1a818bd
Johann Bauer <johann.bauer1@gmail.com>
2017-11-14 17:15:28 +0100

    "Never ascribe to malice that which is caused by greed and ignorance." -- Cal Keegan

--- scrolltext.cpp
+++ scrolltext.cpp
@@ -88,7 +88,7 @@ void ScrollText::paintEvent(QPaintEvent*)
         {
 
             //qDebug() << "DARW";
-            pb.drawStaticText(QPointF(x, (height() - wholeTextSize.height()) / 2) + QPoint(0, 0), staticText);
+            pb.drawStaticText(QPointF(x, (height() - wholeTextSize.height()) / 2) + QPoint(2, -3), staticText);
             x += wholeTextSize.width();
         }
 
